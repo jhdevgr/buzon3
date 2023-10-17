46e55bc42fa16816fd7cfcaee54d1b0a93eb1592
jhdevgr <jhdevgr@gmail.com>
2023-10-17 18:41:22 +0200

    feat(*): change position

---
+++ index.js
@@ -0,0 +1,54 @@
+import Buzon from './buzon.js';
+
+(function() {
+  const persons = [
+    { name: 'Julián', avatar: './img/pacman2.png' },
+    { name: 'Gian', avatar: './img/pacman2.png' },
+    { name: 'Estela', avatar: './img/pacman2.png' },
+    { name: 'Abel', avatar: './img/pacman2.png' },
+  ];
+
+  const $mainImage = document.querySelector('#main-image');
+  const $mainTitle = document.querySelector('#main-title');
+  const $mainDate = document.querySelector('#main-date');
+  const $buzonList = document.querySelector('#buzon-list');
+  const $buzonItemTemplate = document.querySelector('#buzon-item-template');
+
+  const buzon = new Buzon({
+    startDate: '2021-04-07',
+    persons
+  });
+
+  const currentPerson = buzon.getPerson();
+  const cycleStartDate = new Date(buzon.getCycleStartDate().getTime() + buzon.dayLength);
+
+  function dateFormat(date) {
+    return date.toLocaleDateString("es-ES", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }).replace(/^(.)|\s+(.)/g, function (letter) {
+      return letter.toUpperCase()
+    }).replaceAll('De', 'de');
+  }
+
+  $mainImage.src = currentPerson.avatar;
+  $mainTitle.innerText = currentPerson.name;
+  $mainDate.innerText = dateFormat(cycleStartDate);
+
+  const currentPersonIndex = persons.indexOf(currentPerson);
+  const buzonList = [
+    ...persons.slice(currentPersonIndex),
+    ...persons.slice(0, currentPersonIndex)
+  ]
+      .filter((person) => person !== currentPerson)
+      .map((person, index) => ({
+        ...person,
+        startDate: new Date(cycleStartDate.getTime() + (buzon.weekLength * (index + 1)))
+      }));
+  $buzonList.innerHTML = buzonList.map(getItemTemplate).join('');
+
+  function getItemTemplate(person) {
+    return $buzonItemTemplate.innerHTML
+        .split('{{name}}').join(person.name)
+        .split('{{date}}').join(dateFormat(person.startDate))
+        .split('{{image}}').join(person.avatar);
+  }
+
+})();
